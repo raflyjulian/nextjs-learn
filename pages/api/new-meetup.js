8f1ba08db6955dfa1921fd23c28a6e7c1c4925f4
raflyjulian <rafly.julian.internship@nawatech.co>
2025-11-11 19:07:36 +0700

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if(req.method === 'POST'){
+    const data = req.body;
+
+
+    const client = await MongoClient.connect('mongodb+srv://db_latihan:vsPDmG4LA3I91wtD@cluster0.7gii4tl.mongodb.net/meetups?appName=Cluster0');
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup inserted'});
+  }
+}
+
+export default handler;
